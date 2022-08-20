ddbe66378f817518c786bd241787df8005557b0c
cuneytsoylu <110092795+cuneytsoylu@users.noreply.github.com>
2022-08-20 15:01:30 +0300

    Update MakineMuhendisi.java

--- src/InterfacesRadyo/MakineMuhendisi.java
+++ src/InterfacesRadyo/MakineMuhendisi.java
@@ -48,5 +48,6 @@ public static void main(String[] args) {
         System.out.println("");
         System.out.println("--------------------------------");
         System.out.println("*********************************");
+             System.out.println("***********************************************************");
     }
 }
